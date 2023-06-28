3624e2ed9a6bbfe490034e9a0d3642a555692b5e
mazenkouki <mazenlouki@gmail.com>
2023-06-28 17:50:51 +0100

    init

---
+++ cars/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="bg-gray-800 text-white p-4">
+    <h1 className="text-2xl font-bold">Hello, Tailwind CSS!</h1>
+    <p className="text-lg">This is an example component.</p>
+    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4">
+      Click Me
+    </button>
+  </div>
+  );
+}
+
+export default App;
